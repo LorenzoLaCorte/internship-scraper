1071fcb2e688af44389c730bf0db51f960987590
LLaCorte <lorenzolacorte99@gmail.com>
2023-10-16 21:51:27 +0200

    Add Jspy Code

---
+++ src/jobspy/__init__.py
@@ -0,0 +1,176 @@
+import pandas as pd
+import concurrent.futures
+from concurrent.futures import ThreadPoolExecutor
+from typing import Tuple, Optional
+
+from .jobs import JobType, Location
+from .scrapers.indeed import IndeedScraper
+from .scrapers.ziprecruiter import ZipRecruiterScraper
+from .scrapers.linkedin import LinkedInScraper
+from .scrapers import ScraperInput, Site, JobResponse, Country
+from .scrapers.exceptions import (
+    LinkedInException,
+    IndeedException,
+    ZipRecruiterException,
+)
+
+SCRAPER_MAPPING = {
+    Site.LINKEDIN: LinkedInScraper,
+    Site.INDEED: IndeedScraper,
+    Site.ZIP_RECRUITER: ZipRecruiterScraper,
+}
+
+
+def _map_str_to_site(site_name: str) -> Site:
+    return Site[site_name.upper()]
+
+
+def scrape_jobs(
+    site_name: str | list[str] | Site | list[Site],
+    search_term: str,
+    location: str = "",
+    distance: int = None,
+    is_remote: bool = False,
+    job_type: str = None,
+    easy_apply: bool = False,  # linkedin
+    results_wanted: int = 15,
+    country_indeed: str = "usa",
+    hyperlinks: bool = False,
+    proxy: Optional[str] = None,
+    offset: Optional[int] = 0,
+) -> pd.DataFrame:
+    """
+    Simultaneously scrapes job data from multiple job sites.
+    :return: results_wanted: pandas dataframe containing job data
+    """
+
+    def get_enum_from_value(value_str):
+        for job_type in JobType:
+            if value_str in job_type.value:
+                return job_type
+        raise Exception(f"Invalid job type: {value_str}")
+
+    job_type = get_enum_from_value(job_type) if job_type else None
+
+    if type(site_name) == str:
+        site_type = [_map_str_to_site(site_name)]
+    else:  #: if type(site_name) == list
+        site_type = [
+            _map_str_to_site(site) if type(site) == str else site_name
+            for site in site_name
+        ]
+
+    country_enum = Country.from_string(country_indeed)
+
+    scraper_input = ScraperInput(
+        site_type=site_type,
+        country=country_enum,
+        search_term=search_term,
+        location=location,
+        distance=distance,
+        is_remote=is_remote,
+        job_type=job_type,
+        easy_apply=easy_apply,
+        results_wanted=results_wanted,
+        offset=offset,
+    )
+
+    def scrape_site(site: Site) -> Tuple[str, JobResponse]:
+        scraper_class = SCRAPER_MAPPING[site]
+        scraper = scraper_class(proxy=proxy)
+
+        try:
+            scraped_data: JobResponse = scraper.scrape(scraper_input)
+        except (LinkedInException, IndeedException, ZipRecruiterException) as lie:
+            raise lie
+        except Exception as e:
+            # unhandled exceptions
+            if site == Site.LINKEDIN:
+                raise LinkedInException()
+            if site == Site.INDEED:
+                raise IndeedException()
+            if site == Site.ZIP_RECRUITER:
+                raise ZipRecruiterException()
+            else:
+                raise e
+        return site.value, scraped_data
+
+    site_to_jobs_dict = {}
+
+    def worker(site):
+        site_val, scraped_info = scrape_site(site)
+        return site_val, scraped_info
+
+    with ThreadPoolExecutor() as executor:
+        future_to_site = {
+            executor.submit(worker, site): site for site in scraper_input.site_type
+        }
+
+        for future in concurrent.futures.as_completed(future_to_site):
+            site_value, scraped_data = future.result()
+            site_to_jobs_dict[site_value] = scraped_data
+
+    jobs_dfs: list[pd.DataFrame] = []
+
+    for site, job_response in site_to_jobs_dict.items():
+        for job in job_response.jobs:
+            job_data = job.dict()
+            job_data[
+                "job_url_hyper"
+            ] = f'<a href="{job_data["job_url"]}">{job_data["job_url"]}</a>'
+            job_data["site"] = site
+            job_data["company"] = job_data["company_name"]
+            job_data["job_type"] = (
+                ", ".join(job_type.value[0] for job_type in job_data["job_type"])
+                if job_data["job_type"]
+                else None
+            )
+            job_data["emails"] = (
+                ", ".join(job_data["emails"]) if job_data["emails"] else None
+            )
+            job_data["location"] = Location(**job_data["location"]).display_location()
+
+            compensation_obj = job_data.get("compensation")
+            if compensation_obj and isinstance(compensation_obj, dict):
+                job_data["interval"] = (
+                    compensation_obj.get("interval").value
+                    if compensation_obj.get("interval")
+                    else None
+                )
+                job_data["min_amount"] = compensation_obj.get("min_amount")
+                job_data["max_amount"] = compensation_obj.get("max_amount")
+                job_data["currency"] = compensation_obj.get("currency", "USD")
+            else:
+                job_data["interval"] = None
+                job_data["min_amount"] = None
+                job_data["max_amount"] = None
+                job_data["currency"] = None
+
+            job_df = pd.DataFrame([job_data])
+            jobs_dfs.append(job_df)
+
+    if jobs_dfs:
+        jobs_df = pd.concat(jobs_dfs, ignore_index=True)
+        desired_order: list[str] = [
+            "job_url_hyper" if hyperlinks else "job_url",
+            "site",
+            "title",
+            "company",
+            "location",
+            "job_type",
+            "date_posted",
+            "interval",
+            "min_amount",
+            "max_amount",
+            "currency",
+            "is_remote",
+            "num_urgent_words",
+            "benefits",
+            "emails",
+            "description",
+        ]
+        jobs_formatted_df = jobs_df[desired_order]
+    else:
+        jobs_formatted_df = pd.DataFrame()
+
+    return jobs_formatted_df
---
+++ src/jobspy/jobs/__init__.py
@@ -0,0 +1,197 @@
+from typing import Union, Optional
+from datetime import date
+from enum import Enum
+
+from pydantic import BaseModel, validator
+
+
+class JobType(Enum):
+    FULL_TIME = (
+        "fulltime",
+        "períodointegral",
+        "estágio/trainee",
+        "cunormăîntreagă",
+        "tiempocompleto",
+        "vollzeit",
+        "voltijds",
+        "tempointegral",
+        "全职",
+        "plnýúvazek",
+        "fuldtid",
+        "دوامكامل",
+        "kokopäivätyö",
+        "tempsplein",
+        "vollzeit",
+        "πλήρηςαπασχόληση",
+        "teljesmunkaidő",
+        "tempopieno",
+        "tempsplein",
+        "heltid",
+        "jornadacompleta",
+        "pełnyetat",
+        "정규직",
+        "100%",
+        "全職",
+        "งานประจำ",
+        "tamzamanlı",
+        "повназайнятість",
+        "toànthờigian",
+    )
+    PART_TIME = ("parttime", "teilzeit")
+    CONTRACT = ("contract", "contractor")
+    TEMPORARY = ("temporary",)
+    INTERNSHIP = ("internship", "prácticas", "ojt(onthejobtraining)", "praktikum")
+
+    PER_DIEM = ("perdiem",)
+    NIGHTS = ("nights",)
+    OTHER = ("other",)
+    SUMMER = ("summer",)
+    VOLUNTEER = ("volunteer",)
+
+
+class Country(Enum):
+    ARGENTINA = ("argentina", "ar")
+    AUSTRALIA = ("australia", "au")
+    AUSTRIA = ("austria", "at")
+    BAHRAIN = ("bahrain", "bh")
+    BELGIUM = ("belgium", "be")
+    BRAZIL = ("brazil", "br")
+    CANADA = ("canada", "ca")
+    CHILE = ("chile", "cl")
+    CHINA = ("china", "cn")
+    COLOMBIA = ("colombia", "co")
+    COSTARICA = ("costa rica", "cr")
+    CZECHREPUBLIC = ("czech republic", "cz")
+    DENMARK = ("denmark", "dk")
+    ECUADOR = ("ecuador", "ec")
+    EGYPT = ("egypt", "eg")
+    FINLAND = ("finland", "fi")
+    FRANCE = ("france", "fr")
+    GERMANY = ("germany", "de")
+    GREECE = ("greece", "gr")
+    HONGKONG = ("hong kong", "hk")
+    HUNGARY = ("hungary", "hu")
+    INDIA = ("india", "in")
+    INDONESIA = ("indonesia", "id")
+    IRELAND = ("ireland", "ie")
+    ISRAEL = ("israel", "il")
+    ITALY = ("italy", "it")
+    JAPAN = ("japan", "jp")
+    KUWAIT = ("kuwait", "kw")
+    LUXEMBOURG = ("luxembourg", "lu")
+    MALAYSIA = ("malaysia", "malaysia")
+    MEXICO = ("mexico", "mx")
+    MOROCCO = ("morocco", "ma")
+    NETHERLANDS = ("netherlands", "nl")
+    NEWZEALAND = ("new zealand", "nz")
+    NIGERIA = ("nigeria", "ng")
+    NORWAY = ("norway", "no")
+    OMAN = ("oman", "om")
+    PAKISTAN = ("pakistan", "pk")
+    PANAMA = ("panama", "pa")
+    PERU = ("peru", "pe")
+    PHILIPPINES = ("philippines", "ph")
+    POLAND = ("poland", "pl")
+    PORTUGAL = ("portugal", "pt")
+    QATAR = ("qatar", "qa")
+    ROMANIA = ("romania", "ro")
+    SAUDIARABIA = ("saudi arabia", "sa")
+    SINGAPORE = ("singapore", "sg")
+    SOUTHAFRICA = ("south africa", "za")
+    SOUTHKOREA = ("south korea", "kr")
+    SPAIN = ("spain", "es")
+    SWEDEN = ("sweden", "se")
+    SWITZERLAND = ("switzerland", "ch")
+    TAIWAN = ("taiwan", "tw")
+    THAILAND = ("thailand", "th")
+    TURKEY = ("turkey", "tr")
+    UKRAINE = ("ukraine", "ua")
+    UNITEDARABEMIRATES = ("united arab emirates", "ae")
+    UK = ("uk", "uk")
+    USA = ("usa", "www")
+    URUGUAY = ("uruguay", "uy")
+    VENEZUELA = ("venezuela", "ve")
+    VIETNAM = ("vietnam", "vn")
+
+    # internal for ziprecruiter
+    US_CANADA = ("usa/ca", "www")
+
+    # internal for linkeind
+    WORLDWIDE = ("worldwide", "www")
+
+    def __new__(cls, country, domain):
+        obj = object.__new__(cls)
+        obj._value_ = country
+        obj.domain = domain
+        return obj
+
+    @property
+    def domain_value(self):
+        return self.domain
+
+    @classmethod
+    def from_string(cls, country_str: str):
+        """Convert a string to the corresponding Country enum."""
+        country_str = country_str.strip().lower()
+        for country in cls:
+            if country.value == country_str:
+                return country
+        valid_countries = [country.value for country in cls]
+        raise ValueError(
+            f"Invalid country string: '{country_str}'. Valid countries (only include this param for Indeed) are: {', '.join(valid_countries)}"
+        )
+
+
+class Location(BaseModel):
+    country: Country = None
+    city: Optional[str] = None
+    state: Optional[str] = None
+
+    def display_location(self) -> str:
+        location_parts = []
+        if self.city:
+            location_parts.append(self.city)
+        if self.state:
+            location_parts.append(self.state)
+        if self.country and self.country not in (Country.US_CANADA, Country.WORLDWIDE):
+            if self.country.value in ("usa", "uk"):
+                location_parts.append(self.country.value.upper())
+            else:
+                location_parts.append(self.country.value.title())
+        return ", ".join(location_parts)
+
+
+class CompensationInterval(Enum):
+    YEARLY = "yearly"
+    MONTHLY = "monthly"
+    WEEKLY = "weekly"
+    DAILY = "daily"
+    HOURLY = "hourly"
+
+
+class Compensation(BaseModel):
+    interval: Optional[CompensationInterval] = None
+    min_amount: int = None
+    max_amount: int = None
+    currency: Optional[str] = "USD"
+
+
+class JobPost(BaseModel):
+    title: str
+    company_name: str
+    job_url: str
+    location: Optional[Location]
+
+    description: str | None = None
+    job_type: list[JobType] | None = None
+    compensation: Compensation | None = None
+    date_posted: date | None = None
+    benefits: str | None = None
+    emails: list[str] | None = None
+    num_urgent_words: int | None = None
+    is_remote: bool | None = None
+    # company_industry: str | None = None
+
+
+class JobResponse(BaseModel):
+    jobs: list[JobPost] = []
