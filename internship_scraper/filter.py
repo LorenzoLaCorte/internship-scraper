dc70b17c893ebf4bf3498aafc73e0f346b5732ce
LLaCorte <lorenzolacorte99@gmail.com>
2023-11-12 20:02:09 +0100

    Add Filter for Location

--- internship_scraper/filter.py
+++ internship_scraper/filter.py
@@ -5,6 +5,8 @@ from internship_scraper.constants import (
     JOB_TITLES,
     JOB_TYPES,
     RESULTS_FILE,
+    EUROPEAN_CITIES,
+    EUROPEAN_COUNTRIES
 )
 from internship_scraper.utils import dump_filtered_results
 
@@ -15,10 +17,15 @@ def filter_internships() -> None:
         filtered_lines: list[str] = []
         for line in csv_reader:
             title = line["title"]
+            city = line["location"].split(", ")[0]
+            country = line["location"].split(", ")[-1]
+            print(city, country, (any(city == job_city for job_city in EUROPEAN_CITIES)) or (any(country == job_country for job_country in EUROPEAN_COUNTRIES)))
             if (
                 (any(category in title for category in JOB_CATEGORIES))
                 and (any(title in title for title in JOB_TITLES))
                 and (any(job_type in title for job_type in JOB_TYPES))
+                and ((any(city == job_city for job_city in EUROPEAN_CITIES))
+                    or (any(country == job_country for job_country in EUROPEAN_COUNTRIES)))
             ):
                 filtered_lines.append("|".join(line.values()))
     dump_filtered_results(filtered_lines)
