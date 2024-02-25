1d46604b1cf2cbe394a5d21177f2e2e9dfa600bf
LLaCorte <lorenzolacorte99@gmail.com>
2024-02-25 18:53:37 +0100

    Ignore exceptions

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from internship_scraper.scraper import find_internships
 from internship_scraper.utils import csv_to_markdown_table
 
 if __name__ == "__main__":
-    asyncio.run(find_internships())
-    filter_internships()
-    csv_to_markdown_table(FILTERED_RESULTS_FILE, TABLE_FILE)
+    try:
+        asyncio.run(find_internships())
+        filter_internships()
+        csv_to_markdown_table(FILTERED_RESULTS_FILE, TABLE_FILE)
+    except Exception as e:
+        print(f"An error occurred: {e}")
