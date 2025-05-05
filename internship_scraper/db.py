18146031598df080c0a19d57916fa85dc9cca850
LLaCorte <lorenzolacorte99@gmail.com>
2025-05-05 15:42:32 +0200

    Add workflow for weekly reset of jobs table and implement reset functionality

--- internship_scraper/db.py
+++ internship_scraper/db.py
@@ -8,9 +8,11 @@ from sqlalchemy.ext.declarative import declarative_base
 load_dotenv()
 database_url = os.getenv('DATABASE_URL')
 
+
 if not database_url:
     raise ValueError("DATABASE_URL environment variable not set")
 engine = create_engine(database_url, pool_pre_ping=True)
+
 
 Base = declarative_base()
 
@@ -18,6 +20,7 @@ def create_session():
     Session = sessionmaker(bind=engine)
     session = Session()
     return session
+
 
 def create_tables():
     Base.metadata.create_all(engine)
@@ -36,5 +39,24 @@ class Job(Base):
     industries = Column(String)
 
 
+def empty_jobs_table():
+    """
+    Delete all records from the jobs table.
+    """
+    session = create_session()
+    session.query(Job).delete()
+    session.commit()
+    session.close()
+
+
+def reset_jobs_table():
+    """
+    Reset the jobs table by deleting all records and then creating the table again.
+    """
+    empty_jobs_table()
+    create_tables()
+
+
 if __name__ == "__main__":
+    empty_jobs_table()
     create_tables()
